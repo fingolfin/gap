7120c73db1d6b6778b034170af05e11a27ef89db
Max Horn <max@quendi.de>
2016-03-11 11:51:24 +0100

    kernel: removed more HAVE_FOO_H macros
    In particular, removed more of these were we either #include the file
    elsewhere without a guard anyway; or where we have no alternative
    implementation that works without the header.
    
    We should only add these back if / when we encounter platforms where
    they make a difference; and in that case, they should be added back with
    a comment pointing out the systems that need them!

--- src/hpc/gapmpi.c
+++ src/hpc/gapmpi.c
@@ -17,8 +17,9 @@
 
 #include        <assert.h>              /* assert                          */
 #include        <time.h>                /* time                            */
-#include	<stdlib.h>		/* exit				   */
-#include	<stdio.h>		/* NULL, fprintf		   */
+#include        <stdlib.h>              /* exit                            */
+#include        <stdio.h>               /* NULL, fprintf                   */
+
 #include        <src/system.h>              /* system dependent part           */
 
 #include        <src/gasman.h>              /* garbage collector               */
@@ -48,10 +49,8 @@
 
 # include       "gapmpi.h"               /* MPI functions and UNIX utils   */
 #include        <mpi.h>                  /* provided with MPI distribution */
-#ifndef SYS_UNISTD_H                     /* definition of 'chdir'          */
-# include <unistd.h>
-# define SYS_UNISTD_H
-#endif
+
+#include <unistd.h>                     /* definition of 'chdir'          */
 #include <sys/time.h>
 #include <sys/resource.h>
 
