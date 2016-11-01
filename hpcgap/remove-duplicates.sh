e8b4c63891c33227036bc4a7da30b91557697c6e
Max Horn <max@quendi.de>
2016-11-01 16:26:46 +0100

    hpc-merge: add remove-duplicates script

---
+++ hpcgap/remove-duplicates.sh
@@ -0,0 +1,6 @@
+#!/bin/sh -ex
+#
+# Find and remove any files which are identical in the root GAP
+# directory and the hpcgap subdirectory.
+#
+find . -type f -exec cmp -s ../{} {} \; -print0 | xargs -0 git rm --quiet
