94a9bfcd6fca688fb8324426619fb44a0b4015cf
Max Horn <max@quendi.de>
2017-03-14 14:07:37 +0100

    HACK: tweak hpcgap/remove-duplicates.sh

--- hpcgap/remove-duplicates.sh
+++ hpcgap/remove-duplicates.sh
@@ -3,4 +3,4 @@
 # Find and remove any files which are identical in the root GAP
 # directory and the hpcgap subdirectory.
 #
-find . -type f -exec cmp -s ../{} {} \; -print0 | xargs -0 git rm --quiet
+find . -type f -exec cmp -s ../{} {} \; -print0 | xargs -0 git rm --force
