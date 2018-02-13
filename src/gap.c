7a5a04f7114d74ec360cd0be384f27ace9fb1987
Markus Pfeiffer <markus.pfeiffer@st-andrews.ac.uk>
2018-02-13 13:53:35 +0000

    Add IsLIBGAP constant to detect when GAP is run as a library

--- src/gap.c
+++ src/gap.c
@@ -1680,6 +1680,13 @@ static Int InitLibrary (
     AssConstantGVar( GVarName( "IsHPCGAP" ), False );
 #endif
 
+#if defined(LIBGAP)
+    AssConstantGVar( GVarName( "IsLIBGAP" ), True );
+#else
+    AssConstantGVar( GVarName( "IsLIBGAP" ), False );
+#endif
+
+
     /* return success                                                      */
     return PostRestore( module );
 }
